f25d5e5de8103ea5e46b9e3b4eecee995d6dd7a5
Tomislav Jankovic <tomislavjankovic@Tomislavs-MacBook-Pro.local>
2017-03-08 17:05:31 +0100

    newest test

--- JenkinsTut/ViewController.swift
+++ JenkinsTut/ViewController.swift
@@ -15,8 +15,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         
-        let asd = "asd"
-        print(asd)
     }
 
     override func didReceiveMemoryWarning() {
