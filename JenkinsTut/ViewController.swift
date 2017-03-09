50132433b929afa7e96a869305f0369fbcd2e3e5
Tomislav Jankovic <tomislavjankovic@Tomislavs-MacBook-Pro.local>
2017-03-09 10:10:53 +0100

    testing without hook url

--- JenkinsTut/ViewController.swift
+++ JenkinsTut/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("testing hook")
+        
     }
 
     override func didReceiveMemoryWarning() {
