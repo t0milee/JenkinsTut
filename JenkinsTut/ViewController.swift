32097a9e57433a1f4576611838d9bfef2c861371
Tomislav Jankovic <jankovic.tm@gmail.com>
2017-03-14 10:28:23 +0100

    testing Jenkings push workspace

--- JenkinsTut/ViewController.swift
+++ JenkinsTut/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("test")
+        print("testing")
     }
 
     override func didReceiveMemoryWarning() {
