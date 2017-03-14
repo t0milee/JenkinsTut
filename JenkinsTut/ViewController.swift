71b8958b63244a33ab5de44c28a7ab6ab95acf1d
Tomislav Jankovic <jankovic.tm@gmail.com>
2017-03-14 11:17:59 +0100

    testing Jenkings

--- JenkinsTut/ViewController.swift
+++ JenkinsTut/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("test")
+        print("testing")
     }
 
     override func didReceiveMemoryWarning() {
